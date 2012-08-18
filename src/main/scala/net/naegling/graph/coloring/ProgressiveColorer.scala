12a539856c2c4805fd27fc4ec402b8718c992a51
Grant Gould <ggould@alum.mit.edu>
2012-08-18 10:53:35 -0400

    Some statistical collection for estimating performance

--- src/main/scala/net/naegling/graph/coloring/ProgressiveColorer.scala
+++ src/main/scala/net/naegling/graph/coloring/ProgressiveColorer.scala
@@ -3,13 +3,17 @@ package net.naegling.graph.coloring
 import net.naegling.graph.UndirectedGraph
 
 /**
- * A small improvement on the brute force colorer: Try coloring with smaller
+ * A small improvement on the breadth-first colorer: Try coloring with smaller
  * numbers of colors first.  Worse worse-case complexity (because it does
  * strictly more work) but much, much better average case (because low-color
  * solutions are much faster than higher color ones)
+ * 
+ * Although the algorithmic tweak is trivial, this is about two orders of
+ * magnitude better than than the breadth-first colorer at the boundaries of a
+ * 2010-era laptop.
  */
 object ProgressiveColorer extends GraphColorer {
-  
+
   def _colorInternal[Node](
     graph : UndirectedGraph[Node],
     maxColors : Int,
@@ -21,4 +25,12 @@ object ProgressiveColorer extends GraphColorer {
     }
     return None
   }
+
+  override def setStatsCollection(newVal : Boolean) {
+    statsCollection = newVal
+    BreadthFirstColorer.statsCollection = newVal
+  }
+  
+  override def getExtendHypothesisStat = 
+    BreadthFirstColorer.getExtendHypothesisStat
 }
